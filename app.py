fd54975005b82d6249273182642886ed2d61ee9d
Charles Shao <charles.shao@csedge.org>
2024-07-16 21:48:05 -0700

    letter grade

--- app.py
+++ app.py
@@ -21,9 +21,44 @@ def hello():
         weight5 = float(request.form['fifthweight'])
         weight6 = float(request.form['sixthweight'])
         
-        final_grade = str(func.grade_calculator(grade1, weight1, grade2, weight2, grade3, weight3, grade4, weight4, grade5, weight5, grade6, weight6, 0, 0))
+        final_grade = func.grade_calculator(grade1, weight1, grade2, weight2, grade3, weight3, grade4, weight4, grade5, weight5, grade6, weight6, 0, 0)
         
-        return render_template('index.html', result=final_grade)
+        letter = ''
+        if final_grade == 100:
+            letter = 'A+'
+        if final_grade >= 93 and final_grade < 100:
+            letter = 'A'
+        
+        if final_grade >= 90 and final_grade < 93:
+            letter = 'A-'
+        if final_grade >= 87 and final_grade < 90:
+            letter = 'B+'
+        if final_grade >= 83 and final_grade < 87:
+            letter = 'B'
+        if final_grade >= 80 and final_grade < 83:
+            letter = 'B-'
+            
+        if final_grade >= 77 and final_grade < 80:
+            letter = 'C+'
+        if final_grade >= 73 and final_grade < 77:
+            letter = 'C'
+        if final_grade >= 70 and final_grade < 73:
+            letter = 'C-'
+
+        if final_grade >= 67 and final_grade < 70:
+            letter = 'D+'
+        if final_grade >= 63 and final_grade < 67:
+            letter = 'D'
+        if final_grade >= 60 and final_grade < 63:
+            letter = 'D-'
+            
+        if final_grade < 60:
+            letter = 'F'
+            
+        
+    
+        
+        return render_template('index.html', result=str(final_grade), letter=letter)
     return render_template('index.html')
 
 if __name__ == '__main__':
